ec4eb7b9bbcd167bd9f93154fa73e366017ffa7c
azra.roa <azra.roa@pucp.edu.pe>
2026-07-04 21:21:28 -0500

    Unión de los módulos 200, 300 y 500 en una sola base de datos

--- scripts/01_Carga_Unión_Módulos.R
+++ scripts/01_Carga_Unión_Módulos.R
@@ -19,3 +19,17 @@ mod200 <- read_dta("datos/crudos/enaho01-2025-200.dta") %>% rename_with(tolower)
 mod300 <- read_dta("datos/crudos/enaho01a-2025-300.dta") %>% rename_with(tolower)
 mod500 <- read_dta("datos/crudos/enaho01a-2025-500.dta") %>% rename_with(tolower)
 
+#3. Unión de bases----------------------------
+
+#Se procede primero con la creación de llaves para que sea posible la unión entre los módulos
+
+keys_hogar <- c("año", "mes", "conglome", "vivienda", "hogar", "ubigeo", "dominio", 
+                "estrato", "nconglome", "sub_conglome")
+keys_persona <- c(keys_hogar, "codperso", "p203", "p204", "p205", 
+                  "p206", "p207", "p208a", "p209") 
+
+#Se realiza la unión entre los módulos
+
+enaho_2025 <- mod500 %>% 
+  left_join(mod300, by = keys_persona) %>% 
+  left_join(mod200, by = keys_persona)
