ab84ad620ee972ce85edeeb9e563bb324fdd38cb
azra.roa <azra.roa@pucp.edu.pe>
2026-07-05 14:46:56 -0500

    Precisiones en el script con respecto a los modulos utilizados, las keys creadas y la utilizacion del formato parquet

--- scripts/01_Carga_Unión_Módulos.R
+++ scripts/01_Carga_Unión_Módulos.R
@@ -15,18 +15,18 @@ renv::snapshot()
 
 #2. Importación de datos----------------------
 
-mod200 <- read_dta("datos/crudos/enaho01-2025-200.dta") %>% rename_with(tolower) #Se ha utilizado tolower para transformar todos los nombres de variables a minúsculas
-mod300 <- read_dta("datos/crudos/enaho01a-2025-300.dta") %>% rename_with(tolower)
-mod500 <- read_dta("datos/crudos/enaho01a-2025-500.dta") %>% rename_with(tolower)
+mod200 <- read_dta("datos/crudos/enaho01-2025-200.dta") %>% rename_with(tolower) #Importación del módulo 200 "Caracteristicas de los miembros del hogar" de la ENAHO 2025
+mod300 <- read_dta("datos/crudos/enaho01a-2025-300.dta") %>% rename_with(tolower) #Importación del módulo 300 "Educación" de la ENAHO 2025
+mod500 <- read_dta("datos/crudos/enaho01a-2025-500.dta") %>% rename_with(tolower) #Importación del módulo 500 "Empleo e ingresos" de la ENAHO 2025
 
 #3. Unión de bases----------------------------
 
 #Se procede primero con la creación de llaves para que sea posible la unión entre los módulos
 
 keys_hogar <- c("año", "mes", "conglome", "vivienda", "hogar", "ubigeo", "dominio", 
-                "estrato", "nconglome", "sub_conglome")
+                "estrato", "nconglome", "sub_conglome") #Estas variables permiten identificar al hogar
 keys_persona <- c(keys_hogar, "codperso", "p203", "p204", "p205", 
-                  "p206", "p207", "p208a", "p209") 
+                  "p206", "p207", "p208a", "p209") #Estas variables permiten identificar a la persona
 
 #Se realiza la unión entre los módulos
 
@@ -37,4 +37,4 @@ enaho_2025 <- mod500 %>%
 #4. Exportación de la base creada
 library(arrow)
 renv::snapshot()
-write_parquet(enaho_2025, "datos/procesados/enaho_2025.parquet")
+write_parquet(enaho_2025, "datos/procesados/enaho_2025.parquet") #Exportación en formato parquet para reducir el tamaño del archivo
