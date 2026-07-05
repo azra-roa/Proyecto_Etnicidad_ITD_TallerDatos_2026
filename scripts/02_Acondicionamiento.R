3c38e0497665b797c322e4861ca2336ad44feef6
azra.roa <azra.roa@pucp.edu.pe>
2026-07-04 21:56:35 -0500

    Creación del script para acondicionamiento de los datos

---
+++ scripts/02_Acondicionamiento.R
@@ -0,0 +1,9 @@
+#===========================================================================================
+#Proyecto: Análisis de la relacion etnicidad-ITD usando datos de la ENAHO
+#Script: Acondicionamiento
+#Autorxs: Carmen Andonayre y Azra Roa
+#Objetivo: Acondicionamiento de la base de datos (Selección, renombrado,
+#          tratamiento de NAs)
+#Fecha: 04-07-2026
+#==========================================================================================
+
