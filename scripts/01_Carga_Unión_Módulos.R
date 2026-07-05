f55b19c47b1cc85aa108d06d14017c2e1cc7496d
azra.roa <azra.roa@pucp.edu.pe>
2026-07-04 21:04:11 -0500

    Creación del script para carga y unión de modulos de la ENAHO necesarios para el proyecto

---
+++ scripts/01_Carga_Unión_Módulos.R
@@ -0,0 +1,6 @@
+#===========================================================================================
+#Proyecto: Análisis de la relacion etnicidad-ITD usando datos de la ENAHO
+#Autorxs: Carmen Andonayre y Azra Roa
+#Objetivo de este script: Carga de modulos y realizar joins
+#Fecha: 04-07-2026
+#==========================================================================================
