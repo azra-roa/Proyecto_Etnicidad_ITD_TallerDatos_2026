c5e33ce0df02c584e38a3972bb6a4cc5084abd13
azra.roa <azra.roa@pucp.edu.pe>
2026-07-04 20:52:29 -0500

    Creación y ejecución de script de creación de carpetas para el proyecto

---
+++ Creación de carpetas.R
@@ -0,0 +1,16 @@
+#===========================================================================================
+#Proyecto: Análisis de la relacion etnicidad-ITD usando datos de la ENAHO
+#Autorxs: Carmen Andonayre y Azra Roa
+#Objetivo de este script: Crear el sistema de carpetas 
+#Fecha: 04-07-2026
+#==========================================================================================
+
+#Creamos carpetas--------------------------
+
+dir.create("datos")
+dir.create("datos/crudos")
+dir.create("datos/procesados")
+dir.create("outputs")
+dir.create("docs")
+dir.create("scripts")
+
