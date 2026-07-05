b263607635dbc2f15813f8fc1684f475bb5a3617
azra.roa <azra.roa@pucp.edu.pe>
2026-07-04 23:00:50 -0500

    Selección y cambiado de nombre de variables

--- scripts/02_Acondicionamiento.R
+++ scripts/02_Acondicionamiento.R
@@ -7,3 +7,55 @@
 #Fecha: 04-07-2026
 #==========================================================================================
 
+# ------------------------------------------------------------------------------
+# 0. CONFIGURACIÓN DEL ENTORNO--------------------------------------------------
+# ------------------------------------------------------------------------------
+library(tidyverse)
+library(arrow)
+library(janitor)
+library(naniar)
+renv::snapshot()
+
+# ------------------------------------------------------------------------------
+# 1. CARGA, SELECCIÓN y RENOMBRADO---------------------------------
+# ------------------------------------------------------------------------------
+# Carga de la base de datos creada en el script 01 
+enaho_raw <- read_parquet("datos/procesados/enaho_2025.parquet")
+
+#Seleccionamos las variables de interes para el proyecto
+#Se realizará el renombrado de variables en la misma acción
+enaho_seleccion <- enaho_raw %>%
+  select(
+    #Variables de identificación
+    año = año,
+    mes = mes,
+    conglome = conglome, 
+    vivienda = vivienda, 
+    hogar    = hogar, 
+    codperso  = codperso, 
+    ubigeo   = ubigeo, 
+    dominio  = dominio, 
+    estrato  = estrato,
+  
+    #Variables para análisis
+    sexo = p207,
+    etnicidad = p558c,
+    educacion = p301a.y,
+    edad = p208a,
+    pension_no = p558a5,
+    horas_sem = i513t,
+    temp_pago = p523,
+    ing_prin = p524a1,
+    tiene_ruc = p510a1,
+    tiene_contrato = p511a,
+    
+    #Factores de expansión
+    factor200 = facpob07,
+    factor300 = factora07,
+    factor500 = fac500a,
+  )
+
+# Inspección preliminar de la base
+dim(enaho_seleccion)        # Filas y columnas post join y selección
+names(enaho_seleccion)      # Verificación de nombres
+glimpse(enaho_seleccion)    # Revisión crítica de cómo R interpretó los tipos de datos
