c758eb0c93ccbaae7913add0ff6475f2182a9167
azra.roa <azra.roa@pucp.edu.pe>
2026-07-04 21:13:29 -0500

    Importación de los modulos 200, 300 y 500 de la ENAHO para el proyecto

--- scripts/01_Carga_Unión_Módulos.R
+++ scripts/01_Carga_Unión_Módulos.R
@@ -12,3 +12,10 @@ library(haven)
 library(dplyr) 
 library(tidyr)
 renv::snapshot()
+
+#2. Importación de datos----------------------
+
+mod200 <- read_dta("datos/crudos/enaho01-2025-200.dta") %>% rename_with(tolower) #Se ha utilizado tolower para transformar todos los nombres de variables a minúsculas
+mod300 <- read_dta("datos/crudos/enaho01a-2025-300.dta") %>% rename_with(tolower)
+mod500 <- read_dta("datos/crudos/enaho01a-2025-500.dta") %>% rename_with(tolower)
+
