ad5eb8d295927c6322baa985317181f57eb4cb00
carmen2al <carmen.andonayre@pucp.edu.pe>
2026-07-12 19:30:49 -0500

    Creación del script para la exploración de la base de datos de la ENAHO previamente acondicionada.

--- scripts/03_Exploración_Inicial.R
+++ scripts/03_Exploración_Inicial.R
@@ -7,17 +7,3 @@
 
 rm(list = ls())
 
-# ------------------------------------------------------------------------------
-# 0. CONFIGURACIÓN Y CARGA DE DATOS
-# ------------------------------------------------------------------------------
-library(webshot2)
-library(tidyverse)
-library(arrow)
-library(survey)      
-library(srvyr)       
-library(flextable)   
-library(scales)      
-library(officer)
-library(here)
-
-renv::snapshot()
