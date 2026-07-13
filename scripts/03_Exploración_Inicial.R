bff183be29b284493226edd69995b0c94f97b4b3
carmen2al <carmen.andonayre@pucp.edu.pe>
2026-07-12 19:18:59 -0500

    Creación del script para la exploración de la base acondicionada a partir de datos de la ENAHO para el proyecto

---
+++ scripts/03_Exploración_Inicial.R
@@ -0,0 +1,8 @@
+# ====================================================================================
+#Proyecto: Análisis de la relacion etnicidad-ITD usando datos de la ENAHO 
+#Autorxs: Carmen Andonayre y Azra Roa
+#Objetivo de este script: Explorar la base de datos acondicionada
+#Fecha: 12-07-2026
+# =====================================================================================
+
+rm(list = ls())
