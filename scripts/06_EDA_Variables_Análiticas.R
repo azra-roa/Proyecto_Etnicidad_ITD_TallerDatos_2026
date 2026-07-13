d478ccab6758590055324fb15d4683d9a10e12e0
carmen2al <carmen.andonayre@pucp.edu.pe>
2026-07-12 22:56:58 -0500

    Creación de script 6 para el EDA de variables analíticas

---
+++ scripts/06_EDA_Variables_Análiticas.R
@@ -0,0 +1,7 @@
+#===============================================================================
+#Proyecto: Análisis de la relacion etnicidad-ITD usando datos de la ENAHO 
+#Autorxs: Carmen Andonayre y Azra Roa
+#Objetivo: Explorar las variables analíticas creadas
+#Fecha: 12-07-2026
+#===============================================================================
+
